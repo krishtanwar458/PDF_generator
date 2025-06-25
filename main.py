1338f0788b84c93a9b57107c98702a879ece47c7
Krish Tanwar <krishtanwar458@gmail.com>
2025-06-25 12:48:11 +0530

    Added recurring lines on all pages

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ for index, row in df.iterrows():
     pdf.set_font(family='Times', size=8, style='I')
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=10, txt=row['Topic'], ln=1, align='R')
+    #Adding reccuring lines throughout the main pages - METHOD 1
+    b = 32
+    while b < 280:
+        pdf.line(x1=10, x2=200, y1=b, y2=b)
+        b = b+10
 
 #Adding necessary number of pages as taken from the data.csv file and
 #setting footer
@@ -26,6 +31,9 @@ for index, row in df.iterrows():
         pdf.set_font(family='Times', size=8, style='I')
         pdf.set_text_color(100,100,100)
         pdf.cell(w=0, h=10, txt=row['Topic'], ln=1, align='R')
+        #Adding recurring lines throughout the secondary pages - METHOD 2
+        for i in range(15, 280, 10):
+            pdf.line(10, i, 200, i)
 
 pdf.output('output.pdf')
 
