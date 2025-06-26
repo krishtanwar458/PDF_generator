9f3df23099433419e377065cfb3dc4ce23d56ead
Krish Tanwar <krishtanwar458@gmail.com>
2025-06-26 18:31:26 +0530

    added content to the pdf files

--- secondary_project_main.py
+++ secondary_project_main.py
@@ -6,16 +6,18 @@ filepath = glob.glob('/Users/krishtanwar/Desktop/Python/PDF generator/Animal Txt
 
 animals = []
 
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
 for i in filepath:
     animal = Path(i).stem
     # print(animal)
-    animals.append(animal.capitalize())
-    
-pdf = FPDF(orientation='P', unit='mm', format='A4')
-
-for i in animals:
     pdf.add_page()
     pdf.set_font(family='Times', size=16, style='BI')
-    pdf.cell(h=15, w=50, txt=i, ln=1, align='L')
+    pdf.cell(h=15, w=50, txt=animal.capitalize(), ln=1, align='L')
+    pdf.set_font(family='Times', size=10)
+
+    with open(i, 'r') as file:
+        content = file.read()
+    pdf.multi_cell(w=190, h=8, txt=content, align='J')
 
 pdf.output('animal.pdf')
